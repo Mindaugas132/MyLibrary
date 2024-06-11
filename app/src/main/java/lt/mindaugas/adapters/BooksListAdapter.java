8fa84fd085fe74993aac4a2d7c4caf6500057c2d
Mindaugas132 <mindaugas132@outlook.com>
2024-06-11 16:53:45 +0300

    implement Books ViewHolder

--- app/src/main/java/lt/mindaugas/adapters/BooksListAdapter.java
+++ app/src/main/java/lt/mindaugas/adapters/BooksListAdapter.java
@@ -1,9 +1,13 @@
 package lt.mindaugas.adapters;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import lt.mindaugas.mylibrary.R;
 
 public class BooksListAdapter extends RecyclerView.ViewHolder {
     public BooksListAdapter(@NonNull View itemView) {
@@ -12,7 +16,15 @@ public class BooksListAdapter extends RecyclerView.ViewHolder {
 }
 
 class BooksViewHolder extends RecyclerView.ViewHolder {
+    CardView bookContainer;
+    TextView starsView, titleView, authorView;
+
     public BooksViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        bookContainer = itemView.findViewById(R.id.productContainer);
+        starsView = itemView.findViewById(R.id.starsView);
+        titleView = itemView.findViewById(R.id.titleView);
+        authorView = itemView.findViewById(R.id.authorView);
     }
 }
