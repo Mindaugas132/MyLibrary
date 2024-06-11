213f505ae5035dd6b5704c80651ff6505521bffa
Mindaugas132 <mindaugas132@outlook.com>
2024-06-11 16:44:08 +0300

    add Books Adapter and ViewHolder

---
+++ app/src/main/java/lt/mindaugas/adapters/BooksListAdapter.java
@@ -0,0 +1,18 @@
+package lt.mindaugas.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BooksListAdapter extends RecyclerView.ViewHolder {
+    public BooksListAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+}
+
+class BooksViewHolder extends RecyclerView.ViewHolder {
+    public BooksViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
